4a7cc349b9698748f3b71d512d1514e191048d26
sitijdev <sitisolehahbantar@gmail.com>
2024-12-03 04:42:47 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { GOOGLE_IMG_SCRAP, GOOGLE_QUERY } = require('google-img-scrap');
 async function scrapeImages() {
   try {
     const result = await GOOGLE_IMG_SCRAP({
-      search: 'kucing lucu',
+      search: 'sexy lingerie',
       limit: 10,
       query: { // Pastikan menggunakan tanda kurung kurawal
         SIZE: GOOGLE_QUERY.SIZE.LARGE
