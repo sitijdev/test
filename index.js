c57e99e6edee8d24b09857fba98610669a874e76
sitijdev <sitisolehahbantar@gmail.com>
2024-12-02 22:21:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,100 +1,117 @@
-const unirest = require("unirest");
-const cheerio = require("cheerio");
-const fs = require('fs');
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
+puppeteer.use(StealthPlugin());
 
-// Konfigurasi
-const searchUrl = "https://www.google.com/search?q={keywords}&oq={keywords}&hl=en&tbm=isch&asearch=ichunk&async=_id:rg_s,_pms:s,_fmt:pc&sourceid=chrome&ie=UTF-8";
-const keywords = "nike";
-const outputFilename = 'google_images.json'; // Nama file output
-
-
-// Fungsi untuk mendapatkan delay acak antara 1 dan 3 detik
-function getRandomDelay() {
-  return Math.floor(Math.random() * 3000) + 1000;
-}
-
-// Fungsi untuk memilih User-Agent secara acak
-const selectRandomUserAgent = () => {
-  const userAgents = [
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.157 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36",
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
-  ];
-  const randomNumber = Math.floor(Math.random() * userAgents.length);
-  return userAgents[randomNumber];
-};
-
-
-// Fungsi untuk mengambil data gambar dari Google Images
-async function fetchImagesData(url) {
-  let user_agent = selectRandomUserAgent();
-  let header = { "User-Agent": `${user_agent}` };
-
-  try {
-    const response = await unirest
-      .get(url)
-      .headers(header);
-    return response.body; 
-  } catch (error) {
-    console.error('Error saat mengambil data:', error);
-    throw error; // Lempar error agar bisa ditangani di fungsi yang memanggil
+const searchQuery = "bugatti chiron";
+async function getImagesData(page) {
+  const imagesResults = [];
+  let iterationsLength = 0;
+  while (true) {
+    const images = await page.$$(".OcgH4b .PNCib.MSM1fd");
+    for (; iterationsLength < images.length; iterationsLength++) {
+      images[iterationsLength].click();
+      await page.waitForTimeout(2000);
+      imagesResults.push(
+        await page.evaluate(
+          (iterationsLength) => ({
+            thumbnail: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".Q4LuWd")?.getAttribute("src"),
+            source: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".VFACy div")?.textContent.trim(),
+            title: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector("h3")?.textContent.trim(),
+            link: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".VFACy")?.getAttribute("href"),
+            original: Array.from(document.querySelectorAll(".eHAdSb .n3VNCb"))
+              .find((el) => !el.getAttribute("src").includes("data:image") && !el.getAttribute("src").includes("gstatic.com"))
+              ?.getAttribute("src"),
+          }),
+          iterationsLength
+        )
+      );
+    }
+    await page.waitForTimeout(5000);
+    const newImages = await page.$$(".OcgH4b .PNCib.MSM1fd");
+    if (newImages.length === images.length) break;
   }
+  return imagesResults;
 }
 
-
-// Fungsi untuk memproses data HTML dan mengekstrak informasi gambar
-function extractImageData(html) {
-  let $ = cheerio.load(html);
-  let images_results = [];
-
-  $("div.rg_bx").each(async (i, el) => {
-    let json_string = $(el).find(".rg_meta").text();
-    let imageData = JSON.parse(json_string);
-
-    images_results.push({
-      title: $(el).find(".iKjWAf .mVDMnf").text(),
-      source: $(el).find(".iKjWAf .FnqxG").text(),
-      link: imageData.ru || '',
-      original: imageData.ou || '',
-      thumbnail: $(el).find(".rg_l img").attr("src") 
-                || $(el).find(".rg_l img").attr("data-src") 
-                || '',
-    });
-
-    await new Promise(resolve => setTimeout(resolve, getRandomDelay()));
+async function getGoogleImagesResults() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
   });
 
-  return images_results;
+  const URL = `https://www.google.com/search?q=${encodeURI(searchQuery)}&tbm=isch&hl=en&gl=es`;
+
+  const page = await browser.newPage();
+
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto(URL);
+  await page.waitForSelector(".PNCib");
+
+  const imagesResults = await getImagesData(page);
+
+  await browser.close();
+
+  return imagesResults;
 }
 
+getGoogleImagesResults().then((result) => console.dir(result, { depth: null }));
+const puppeteer = require("puppeteer-extra");
+const StealthPlugin = require("puppeteer-extra-plugin-stealth");
 
-// Fungsi untuk menyimpan data gambar ke file JSON
-function logImageData(data) {
-  try {
-    console.log(data);
-  } catch (error) {
-    console.error('Error saat menampilkan data:', error);
+puppeteer.use(StealthPlugin());
+
+const searchQuery = "bugatti chiron";
+
+async function getImagesData(page) {
+  const imagesResults = [];
+  let iterationsLength = 0;
+  while (true) {
+    const images = await page.$$(".OcgH4b .PNCib.MSM1fd");
+    for (; iterationsLength < images.length; iterationsLength++) {
+      images[iterationsLength].click();
+      await page.waitForTimeout(2000);
+      imagesResults.push(
+        await page.evaluate(
+          (iterationsLength) => ({
+            thumbnail: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".Q4LuWd")?.getAttribute("src"),
+            source: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".VFACy div")?.textContent.trim(),
+            title: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector("h3")?.textContent.trim(),
+            link: document.querySelectorAll(".OcgH4b .PNCib.MSM1fd")[iterationsLength].querySelector(".VFACy")?.getAttribute("href"),
+            original: Array.from(document.querySelectorAll(".eHAdSb .n3VNCb"))
+              .find((el) => !el.getAttribute("src").includes("data:image") && !el.getAttribute("src").includes("gstatic.com"))
+              ?.getAttribute("src"),
+          }),
+          iterationsLength
+        )
+      );
+    }
+    await page.waitForTimeout(5000);
+    const newImages = await page.$$(".OcgH4b .PNCib.MSM1fd");
+    if (newImages.length === images.length) break;
   }
+  return imagesResults;
 }
 
+async function getGoogleImagesResults() {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
 
-// Fungsi utama untuk menjalankan proses scraping
-async function scrapeImages() {
-  const url = searchUrl.replace(/{keywords}/g, keywords);
+  const URL = `https://www.google.com/search?q=${encodeURI(searchQuery)}&tbm=isch&hl=en&gl=es`;
 
-  try {
-    const html = await fetchImagesData(url);
-    const imagesData = extractImageData(html);
-    logImageData(imagesData); // <-- Panggil fungsi logImageData
-  } catch (error) {
-    console.error('Proses scraping gagal:', error);
-  }
+  const page = await browser.newPage();
+
+  await page.setDefaultNavigationTimeout(60000);
+  await page.goto(URL);
+  await page.waitForSelector(".PNCib");
+
+  const imagesResults = await getImagesData(page);
+
+  await browser.close();
+
+  return imagesResults;
 }
 
-
-// Jalankan scraper
-scrapeImages();
+getGoogleImagesResults().then((result) => console.dir(result, { depth: null }));
