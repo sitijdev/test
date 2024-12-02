64361b3c9d440105997b65453a543fc4763ce522
sitijdev <sitisolehahbantar@gmail.com>
2024-12-03 04:52:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ async function scrapeImages() {
   try {
     const result = await GOOGLE_IMG_SCRAP({
       search: 'gadis cantik',
-      excludeDomains: ['istockphoto.com', 'fb.com', 'instagram.com', 'facebook.com', 'tiktok.com', 'alamy.com'],
+      excludeDomains: ['istockphoto.com', 'fb.com', 'pinimg.com', 'pinterest.com', 'instagram.com', 'facebook.com', 'tiktok.com', 'alamy.com'],
       limit: 10,
       query: { // Pastikan menggunakan tanda kurung kurawal
         SIZE: GOOGLE_QUERY.SIZE.LARGE
