586360e0ab4b850f48913cc0b4f56be1dedefa57
sitijdev <sitisolehahbantar@gmail.com>
2024-12-03 04:33:15 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const { GOOGLE_IMG_SCRAP } = require('google-img-scrap');
 async function scrapeImages() {
   try {
     const result = await GOOGLE_IMG_SCRAP({
-      search: 'kucing lucu',
-      limit: 10
-      query: {
+  search: 'kucing lucu',
+  limit: 10,
+  query: {
     SIZE: GOOGLE_QUERY.SIZE.LARGE,
     TYPE: GOOGLE_QUERY.TYPE.PHOTO
   }
-    });
+});
 
     console.log(result);
 
