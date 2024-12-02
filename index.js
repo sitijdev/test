fadb9b6a8537ae0ca8e378c1d16c554b19209d74
sitijdev <sitisolehahbantar@gmail.com>
2024-12-03 04:38:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const { GOOGLE_IMG_SCRAP, GOOGLE_QUERY } = require('google-img-scrap');
 async function scrapeImages() {
   try {
     const result = await GOOGLE_IMG_SCRAP({
-  search: 'kucing lucu',
-  limit: 10,
-  query: {
-    SIZE: GOOGLE_QUERY.SIZE.LARGE,
-    TYPE: GOOGLE_QUERY.TYPE.PHOTO
-  }
-});
+      search: 'kucing lucu',
+      limit: 10,
+      query: { // Pastikan menggunakan tanda kurung kurawal
+        SIZE: GOOGLE_QUERY.SIZE.LARGE,
+        TYPE: GOOGLE_QUERY.TYPE.PHOTO
+      }
+    });
 
     console.log(result);
 
